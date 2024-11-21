a100be491bcd86fa488fa3af110abf2344395b00
PaulQuiDev <paul.raguet@edu.devinci.fr>
2024-11-21 15:50:08 +0100

    config web

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",  // <=== enables static exports
-  reactStrictMode: true,
+    reactStrictMode: true,
 };
 
-module.exports = nextConfig;
+
+export default nextConfig;
 
